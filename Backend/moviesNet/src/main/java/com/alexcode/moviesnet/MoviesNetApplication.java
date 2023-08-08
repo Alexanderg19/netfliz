5cc3f0720f30124c60c84f5ad3c691c41fd0b4d7
Alexander <alexander.gallol1020@gmail.com>
2023-08-08 10:38:29 -0500

    Inicializando proyecto backend con Java

---
+++ Backend/moviesNet/src/main/java/com/alexcode/moviesnet/MoviesNetApplication.java
@@ -0,0 +1,13 @@
+package com.alexcode.moviesnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesNetApplication.class, args);
+	}
+
+}
---
+++ Backend/moviesNet/src/test/java/com/alexcode/moviesnet/MoviesNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexcode.moviesnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
